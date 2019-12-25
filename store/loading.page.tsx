5db89b623e7d0df4037bf2f614630a9786c7567d
Kyle Nazario <kyle@servicecore.com>
2019-12-25 11:22:51 -0700

    Removes old nav library code

--- store/loading.page.tsx
+++ store/loading.page.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {View, Text} from 'react-native';
-import {Navigation} from 'react-native-navigation';
 
 const LoadingPage = () => {
   console.log('loading page')
@@ -10,7 +9,5 @@ const LoadingPage = () => {
     </View>
   );
 };
-
-Navigation.registerComponent('LoadingPage', () => LoadingPage);
 
 export default LoadingPage;
