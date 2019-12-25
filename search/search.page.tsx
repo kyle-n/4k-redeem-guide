f0b0d31c3ddd3fa5dc54a5fabc3b4f59b7716dd6
Kyle Nazario <kyle@servicecore.com>
2019-12-25 15:20:48 -0700

    Search page files

---
+++ search/search.page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const SearchPage = () => {
+  console.log('searching page')
+  return (
+    <View>
+      <Text>Search</Text>
+    </View>
+  );
+};
+
+export default SearchPage;
