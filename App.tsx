6a1753299abca8d8e2703893d8e6d0bf0e3233b2
Kyle Nazario <kyle@servicecore.com>
2019-12-25 11:23:14 -0700

    New library code for nav

--- App.tsx
+++ App.tsx
@@ -1,38 +1,12 @@
 import React from 'react';
-import {
-  View,
-  Text,
-} from 'react-native';
-import {Navigation} from 'react-native-navigation';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import {LoadingPage} from './store';
 
-Navigation.events().registerAppLaunchedListener(async () => {
-  console.log('launched')
-  await Navigation.setRoot({
-    root: {
-      stack: {
-        children: [
-          {component: {name: 'LoadingPage'}}
-        ]
-      }
-    }
-  });
-  await Navigation.push('LoadingPage', {
-    component: {
-      name: 'LoadingPage',
-      options: {
-        topBar: {title: {text: 'Loading..'}}
-      }
-    }
-  });
+const MainNavigator = createStackNavigator({
+  Loading: {screen: LoadingPage}
 });
 
-const App = () => {
-  console.log('rendered')
-  return (
-    <View>
-      <Text>Hello 3</Text>
-    </View>
-  );
-};
+const App = () => createAppContainer(MainNavigator);
 
 export default App;
