7053793f2f70f349e4de33c8b7ba4f14c346122d
Kyle Nazario <kyle@servicecore.com>
2019-12-25 15:21:36 -0700

    App component initializes navigation

--- App.tsx
+++ App.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import {LoadingPage} from './store';
+import SearchPage from './search/search.page';
 
 const MainNavigator = createStackNavigator({
-  Loading: {screen: LoadingPage}
+  Home: SearchPage,
+  Startup: LoadingPage
 });
+const AppContainer = createAppContainer(MainNavigator);
 
-const App = () => createAppContainer(MainNavigator);
+const App = () => (
+  <AppContainer />
+);
 
 export default App;
