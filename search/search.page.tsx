d7e6a866e3c5a7e64c61c7e9a250699077401d57
Kyle Nazario <kyle@servicecore.com>
2019-12-25 22:14:36 -0700

    Search page bounces user on startup

--- search/search.page.tsx
+++ search/search.page.tsx
@@ -2,17 +2,44 @@ import React from 'react';
 import {Button, Text, View} from 'react-native';
 import LoadingRedirect from './loading-redirect';
 import {getMovies} from '../store';
+import {NavigationStackScreenProps} from 'react-navigation-stack';
+import {Movie} from '../models';
 
-const SearchPage = (props: any) => {
-  const navToLoadingPage = () => props.navigation.navigate('LoadingPage');
-  console.log(getMovies().length)
-  return (
-    <View>
-      <LoadingRedirect redirect={navToLoadingPage}/>
-      <Text>Search</Text>
-      <Button title="Go load" onPress={() => props.navigation.navigate('LoadingPage')} />
-    </View>
-  );
-};
+type SearchPageProps = NavigationStackScreenProps;
+type SearchPageState = {
+  movies: Movie[]
+}
+
+class SearchPage extends React.Component<SearchPageProps, SearchPageState> {
+  constructor(props: SearchPageProps) {
+    super(props);
+
+    this.state = {movies: []};
+
+    props.navigation.addListener('willFocus', () => {
+      const movies = getMovies();
+      if (!movies.length) {
+        this.navToLoadingPage();
+      } else this.setState({movies});
+    });
+  }
+
+  navToLoadingPage = () => this.props.navigation.navigate('LoadingPage');
+
+  render() {
+    return (
+      <View>
+        <LoadingRedirect redirect={this.navToLoadingPage}/>
+        <Text>Search</Text>
+        <Button title="Go load" onPress={this.navToLoadingPage} />
+        {this.state.movies.map(movie => {
+          return (
+            <Text key={movie.title}>{movie.title}</Text>
+          );
+        })}
+      </View>
+    );
+  }
+}
 
 export default SearchPage;
