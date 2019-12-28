83e4a8133ef2d2773a2f3f72ec87956f1d3c13c3
kyle-n <kylebnazario@gmail.com>
2019-12-28 08:13:53 -0700

    Testing with the best Marvel movie

---
+++ test-data/black-panther.ts
@@ -0,0 +1,23 @@
+import {Movie} from '../models';
+
+export const testMovie: Movie = {
+  imageUrl: '../bp-poster.jpg',
+  title: 'Black Panther (2019)',
+  studio: 'Marvel Studios',
+  maCodeLocation: 'Redeem on Movies anywhere to get a code',
+  vuduFandangoCodeLocation: '',
+  vuduUhd: true,
+  fandangoNowUhd: true,
+  itunesUhd: true,
+  dolbyVision: true,
+  hdr: true,
+  itunesCodeRedeemsUhd: true,
+  moviesAnywhere: true,
+  knownIssues: 'Too good of a movie',
+  modifiedAt: new Date(),
+  uhdVerificationNumber: 1,
+  googlePlayUhd: true,
+  amazonVideoUhd: true,
+  microsoftUhd: true
+};
+
