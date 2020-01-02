1ffa9e6fd9ef5994086953a390dc77f551a2927f
kyle-n <kylebnazario@gmail.com>
2020-01-01 23:33:22 -0500

    [WIP] Search algorithm

--- store/movies.store.ts
+++ store/movies.store.ts
@@ -16,13 +16,25 @@ const transformToMatchableText = (s: string): string => {
   return s.replace(/[\s-]/g, '').toLowerCase();
 };
 
+// need to group property types - iterate over all text props the same way, all booleans, etc
 export const searchMovies = (query: string): Movie[] => {
   const transformedQuery = transformToMatchableText(query);
   const results: Movie[] = [];
   for (let i = 0; i < movies.length; i++) {
     if (transformToMatchableText(movies[i].title).includes(transformedQuery)) {
       results.push(movies[i]);
+      continue;
     }
+
+    let continueLoop = false;
+    for (let q = 0; q < Object.keys(movies[i]).length; q++) {
+      if (transformToMatchableText(Object.keys(movies[i])[q]).includes(transformedQuery)) {
+        results.push(movies[i]);
+        continueLoop = true;
+        break;
+      }
+    }
+    if (continueLoop) continue;
   }
 
   return results;
