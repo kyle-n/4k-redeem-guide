63950738bdf92210e712e7da84ad98657d7e9e96
kyle-n <kylebnazario@gmail.com>
2020-01-01 22:26:19 -0500

    New test movie

---
+++ test-data/bvs.ts
@@ -0,0 +1,23 @@
+import {Movie} from '../models';
+
+export const bvsTestMovie: Movie = {
+  imageUrl: 'http://images2.vudu.com/poster2/876950',
+  title: 'Batman v. Superman: Dawn of Justice',
+  year: 2016,
+  studio: 'Warner Bros.',
+  maCodeLocation: 'N/A',
+  vuduFandangoCodeLocation: 'Redeem on one of the HBO streaming services',
+  vuduUhd: true,
+  fandangoNowUhd: true,
+  itunesUhd: true,
+  dolbyVision: true,
+  hdr: false,
+  itunesCodeRedeemsUhd: true,
+  moviesAnywhere: true,
+  knownIssues: 'Pretty garbage',
+  modifiedAt: new Date(),
+  uhdVerificationNumber: 1,
+  googlePlayUhd: false,
+  amazonVideoUhd: true,
+  microsoftUhd: true
+};
