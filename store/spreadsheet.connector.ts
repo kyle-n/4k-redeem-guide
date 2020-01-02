834ce543e971e77962d3b7b1a8acb884b632cd57
kyle-n <kylebnazario@gmail.com>
2020-01-01 22:27:28 -0500

    Adds new test movie to data

--- store/spreadsheet.connector.ts
+++ store/spreadsheet.connector.ts
@@ -1,6 +1,7 @@
 import {getWorksheet} from './gsheets.js';
 import {GoogleSheetsResponse, Movie, sheetMovieToMovie} from '../models';
 import {testMovie} from '../test-data/black-panther';
+import {bvsTestMovie} from '../test-data/bvs';
 
 const spreadsheetId = '1ZsJkCX4DIv2oeCKQ7zp2ArkR1qKEXCUuCMBbUtHtmh4';
 const worksheetTitle = 'All';
@@ -8,7 +9,7 @@ const worksheetTitle = 'All';
 const loadMovies = async (): Promise<Movie[]> => {
   // const resp: GoogleSheetsResponse = await getWorksheet(spreadsheetId, worksheetTitle);
   // return resp.data.map(sheetMovieToMovie);
-  return [testMovie];
+  return [testMovie, bvsTestMovie];
 };
 
 export default loadMovies;
