d837774565f14304ee4a4ef875e1eef572be8e1b
kyle-n <kylebnazario@gmail.com>
2020-01-01 23:26:40 -0500

    Correctly filters movies by title

--- search/results-container.tsx
+++ search/results-container.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import {Movie} from '../models';
-import {Item} from 'native-base';
+import {Item, Text} from 'native-base';
 import MovieCard from './movie-card';
 import {StyleSheet} from 'react-native';
-import {testMovie} from '../test-data/black-panther';
-import {bvsTestMovie} from '../test-data/bvs';
+import {searchMovies} from '../store';
 
 const resultsContainerStyles = StyleSheet.create({
   container: {
@@ -31,7 +30,8 @@ class ResultsContainer extends React.Component<ResultsContainerProps, ResultsCon
     prevProps: Readonly<ResultsContainerProps>
   ): void {
     if (prevProps !== this.props && this.props.query) {
-      this.setState({movies: [testMovie, bvsTestMovie]});
+      const movies = searchMovies(this.props.query);
+      this.setState({movies});
     }
   }
 
--- store/movies.store.ts
+++ store/movies.store.ts
@@ -11,3 +11,19 @@ export const initializeStore = async (): Promise<void> => {
 export const getMovies = (): Movie[] => {
   return movies.slice();
 };
+
+const transformToMatchableText = (s: string): string => {
+  return s.replace(/[\s-]/g, '').toLowerCase();
+};
+
+export const searchMovies = (query: string): Movie[] => {
+  const transformedQuery = transformToMatchableText(query);
+  const results: Movie[] = [];
+  for (let i = 0; i < movies.length; i++) {
+    if (transformToMatchableText(movies[i].title).includes(transformedQuery)) {
+      results.push(movies[i]);
+    }
+  }
+
+  return results;
+};
