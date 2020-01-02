0e04f1c509ac73d1fa76f36ab46013caec89ce83
kyle-n <kylebnazario@gmail.com>
2020-01-01 19:34:49 -0500

    Composing css on icons

--- search/checkmark-chart.tsx
+++ search/checkmark-chart.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Button, Icon, Left, List, ListItem, Right, Text, View} from 'native-base';
 import {Movie, MoviePropertyDisplayPair} from '../models';
+import {baseFontSize} from '../styles';
+import {StyleProp, StyleSheet} from 'react-native';
 
 const checkmarkValues: MoviePropertyDisplayPair[] = [
   {
@@ -71,11 +73,27 @@ type MoviePropertyStatusProps = {
 const MoviePropertyStatus = (props: MoviePropertyStatusProps) => (
   <ListItem>
     <Left>
-      <Text>{props.displayName}</Text>
+      <Text note={!props.value}>{props.displayName}</Text>
     </Left>
     <Checkmark true={props.value}/>
   </ListItem>
 );
+
+const checkmarkStyles: StyleProp<any> = StyleSheet.create({
+  icon: {
+    backgroundColor: 'rgba(0,0,0,0)',
+    display: 'flex',
+    justifyContent: 'center',
+  }
+});
+
+const yesColors: StyleProp<any> = StyleSheet.create({
+  icon: {
+    color: 'green'
+  }
+});
+
+const yesStyles: StyleProp<any> = StyleSheet.compose(checkmarkStyles, yesColors);
 
 type CheckmarkProps = {
   true: boolean;
@@ -83,11 +101,12 @@ type CheckmarkProps = {
 
 const Checkmark = (props: CheckmarkProps) => (
   <Right>
-    {props.true ? (
-      <Icon name="ios-checkmark-circle" style={{backgroundColor: 'rgba(0,0,0,0)', color: 'green'}} />
-    ) : (
-      <Icon name="close" style={{backgroundColor: 'rgba(0,0,0,0)', color: 'lightgray'}} />
-    )}
+      {props.true ? (
+        <Icon ios="ios-checkmark-circle" android="md-checkmark"
+              style={yesStyles}/>
+      ) : (
+        <Icon ios="ios-close" android="md-close" type="Ionicons" style={checkmarkStyles.icon}/>
+      )}
   </Right>
 );
 
