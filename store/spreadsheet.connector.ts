3b4ae5db20905fd139dc076017924aaaf0351f3b
kyle-n <kylebnazario@gmail.com>
2020-01-01 19:35:00 -0500

    Temp code for testing layouts

--- store/spreadsheet.connector.ts
+++ store/spreadsheet.connector.ts
@@ -1,12 +1,14 @@
 import {getWorksheet} from './gsheets.js';
 import {GoogleSheetsResponse, Movie, sheetMovieToMovie} from '../models';
+import {testMovie} from '../test-data/black-panther';
 
 const spreadsheetId = '1ZsJkCX4DIv2oeCKQ7zp2ArkR1qKEXCUuCMBbUtHtmh4';
 const worksheetTitle = 'All';
 
 const loadMovies = async (): Promise<Movie[]> => {
-  const resp: GoogleSheetsResponse = await getWorksheet(spreadsheetId, worksheetTitle);
-  return resp.data.map(sheetMovieToMovie);
+  // const resp: GoogleSheetsResponse = await getWorksheet(spreadsheetId, worksheetTitle);
+  // return resp.data.map(sheetMovieToMovie);
+  return [testMovie];
 };
 
 export default loadMovies;
