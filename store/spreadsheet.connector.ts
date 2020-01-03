39931a7d61ffd48e08c5c0baaba10853241be8ac
kyle-n <kylebnazario@gmail.com>
2020-01-03 12:26:47 -0500

    Reverts to downloading full movie db

--- store/spreadsheet.connector.ts
+++ store/spreadsheet.connector.ts
@@ -7,9 +7,8 @@ const spreadsheetId = '1ZsJkCX4DIv2oeCKQ7zp2ArkR1qKEXCUuCMBbUtHtmh4';
 const worksheetTitle = 'All';
 
 const loadMovies = async (): Promise<Movie[]> => {
-  // const resp: GoogleSheetsResponse = await getWorksheet(spreadsheetId, worksheetTitle);
-  // return resp.data.map(sheetMovieToMovie);
-  return [testMovie, bvsTestMovie];
+  const resp: GoogleSheetsResponse = await getWorksheet(spreadsheetId, worksheetTitle);
+  return resp.data.map(sheetMovieToMovie);
 };
 
 export default loadMovies;
