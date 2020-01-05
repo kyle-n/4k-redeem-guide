c46d25f683b2f58d4cbd01cbe1b8e9b68dbd961e
kyle-n <kylebnazario@gmail.com>
2020-01-05 17:50:59 -0500

    Movie store caches movies

--- store/movies.store.ts
+++ store/movies.store.ts
@@ -1,10 +1,25 @@
 import loadMovies from './spreadsheet.connector';
+import AsyncStorage from '@react-native-community/async-storage';
 import {Movie} from '../models';
 
 let movies: Movie[] = [];
 
 export const initializeStore = async (): Promise<void> => {
-  movies = await loadMovies();
+  const downloadMovies = async (): Promise<void> => {
+    console.log('downloading movies')
+    movies = await loadMovies();
+    await AsyncStorage.setItem('movies', JSON.stringify(movies));
+  };
+  
+  try {
+    const storedMovies: string | null = await AsyncStorage.getItem('movies');
+    if (storedMovies?.length) {
+      movies = JSON.parse(<string>storedMovies);
+      console.log('loaded stored movies', storedMovies?.length)
+    } else await downloadMovies();
+  } catch {
+    await downloadMovies();
+  }
   return;
 };
 
@@ -12,14 +27,20 @@ export const getMovies = (): Movie[] => {
   return movies.slice();
 };
 
-const transformToMatchableText = (s: string): string => {
-  return s.replace(/[\s-]/g, '').toLowerCase();
-};
 
 // need to group property types - iterate over all text props the same way, all booleans, etc
 export const searchMovies = (query: string): Movie[] => {
+
+  // helpers
+  const transformToMatchableText = (s: string): string => {
+    return s.replace(/[\s-]/g, '').toLowerCase();
+  };
+
+  // setup
   const transformedQuery = transformToMatchableText(query);
   const results: Movie[] = [];
+
+  // iterate over movies
   for (let i = 0; i < movies.length; i++) {
     if (transformToMatchableText(movies[i].title).includes(transformedQuery)) {
       results.push(movies[i]);
