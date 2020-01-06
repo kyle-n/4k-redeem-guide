73c72270bff4c09eb3c450b05f59ca79ac030c38
kyle-n <kylebnazario@gmail.com>
2020-01-05 21:00:01 -0500

    Formatting for suggested searches

--- search/suggested-searches.tsx
+++ search/suggested-searches.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Button, View, Text} from 'native-base';
 import {PresetSearch} from '../models';
+import {baseFontSize} from '../styles'
 
 const presetSearches: PresetSearch[] = [
 	{
-		title: '"Batman" movies',
-		query: 'Batman'
+		title: '"John Wick" movies',
+		query: 'John Wick'
 	},
 	{
 		title: 'Movies from 1974',
@@ -22,7 +23,7 @@ type SuggestedSearchesProps = {
 };
 
 const SuggestedSearches = (props: SuggestedSearchesProps) => (
-	<View>
+	<View style={{alignSelf: 'stretch', display: 'flex', flexDirection: 'column', alignItems: 'flex-start'}}>
 		{presetSearches.map(presetSearch => {
 			return (
 				<SuggestedSearch presetSearch={presetSearch}
@@ -39,7 +40,9 @@ type SuggestedSearchProps = {
 };
 
 const SuggestedSearch = (props: SuggestedSearchProps) => (
-	<Button onPress={() => props.onPress(props.presetSearch)} rounded light>
+	<Button onPress={() => props.onPress(props.presetSearch)}
+					style={{marginVertical: 0.5 * baseFontSize, marginHorizontal: baseFontSize}}
+					rounded light>
 		<Text>{props.presetSearch.title}</Text>
 	</Button>
 );
