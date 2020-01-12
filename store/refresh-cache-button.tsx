79287fe99597fa3e4af1c29543592fd730c17e48
kyle-n <kylebnazario@gmail.com>
2020-01-12 13:08:10 -0700

    Confirm functions on refresh

--- store/refresh-cache-button.tsx
+++ store/refresh-cache-button.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import {Button, Icon} from 'native-base';
+import {Alert} from 'react-native';
+import {clearMovieCache} from './movies.store';
 
 type RefreshCacheButtonProps = {};
 
-const RefreshCacheButton = (props: RefreshCacheButtonProps) => {
-  const showAlert = () => {};
+const RefreshCacheButton = () => {
+  const clearCacheAndNavToLoadingPage = async () => {
+    await clearMovieCache();
+  };
+  const showAlert = () => {
+    Alert.alert(
+      'Refresh movie catalog?',
+      'It is recommended to do this on a good internet connection.',
+      [
+        {text: 'Cancel', style: 'cancel'},
+        {text: 'OK', style: 'default', onPress: clearCacheAndNavToLoadingPage}
+      ]
+    );
+  };
   return (
     <Button onPress={showAlert}
             info transparent large>
