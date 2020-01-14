cee50544ed9782c59815eca856bb0757226f38de
kyle-n <kylebnazario@gmail.com>
2020-01-13 18:53:59 -0700

    Converts camera button back to stateless component

--- barcode-lookup/camera.page.tsx
+++ barcode-lookup/camera.page.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import {NavigationStackScreenProps} from 'react-navigation-stack';
 
+const androidCamPermissions = {
+  title: 'Permission to use camera',
+  message: 'The camera is used to scan the barcode on a movie\'s box.',
+  buttonPositive: 'OK',
+  buttonNegative: 'Cancel'
+};
+
 type CameraPageProps = NavigationStackScreenProps;
 
 const CameraPage = (props: CameraPageProps) => (
