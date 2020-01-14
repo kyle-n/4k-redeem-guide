0c4c7a6bb4ba110be715df6d372309063b9e34e0
kyle-n <kylebnazario@gmail.com>
2020-01-13 19:03:55 -0700

    .

--- barcode-lookup/camera.page.tsx
+++ barcode-lookup/camera.page.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
 import {NavigationStackScreenProps} from 'react-navigation-stack';
+import {View} from 'native-base';
+import {StyleSheet} from 'react-native';
+import {RNCamera} from 'react-native-camera';
+
+const cameraPageStyles = StyleSheet.create({
+  fullSize: {
+    flexGrow: 1,
+    alignSelf: 'stretch'
+  },
+  container: {
+    display: 'flex',
+    flexDirection: 'column'
+  },
+});
 
 const androidCamPermissions = {
   title: 'Permission to use camera',
@@ -8,10 +22,22 @@ const androidCamPermissions = {
   buttonNegative: 'Cancel'
 };
 
-type CameraPageProps = NavigationStackScreenProps;
+type CameraPageProps = NavigationStackScreenProps & {
+  onBarCodeRead: () => void;
+};
 
-const CameraPage = (props: CameraPageProps) => (
-
-);
+const CameraPage = (props: CameraPageProps) => {
+  let cameraRef: RNCamera | null;
+  return (
+    <View style={[cameraPageStyles.container, cameraPageStyles.fullSize]}>
+      <RNCamera style={cameraPageStyles.fullSize}
+                ref={ref => cameraRef = ref}
+                type={RNCamera.Constants.Type.back}
+                androidCameraPermissionOptions={androidCamPermissions}
+                onBarCodeRead={props.onBarCodeRead}
+      />
+    </View>
+  );
+};
 
 export default CameraPage;
