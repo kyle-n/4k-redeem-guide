8157f0a721a5b4d15cb25fd98106708ed07ab687
kyle-n <kylebnazario@gmail.com>
2020-01-13 18:44:15 -0700

    Camera page file setup

---
+++ barcode-lookup/camera.page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {NavigationStackScreenProps} from 'react-navigation-stack';
+
+type CameraPageProps = NavigationStackScreenProps;
+
+const CameraPage = (props: CameraPageProps) => (
+
+);
+
+export default CameraPage;
