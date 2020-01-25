527d8af5d7d3fd2519cfcc9d52ade14f25704fef
kyle-n <kylebnazario@gmail.com>
2020-01-25 11:03:55 -0700

    Simplifies data download check

--- loading/check-before-download.tsx
+++ loading/check-before-download.tsx
@@ -1,34 +1,26 @@
 import React from 'react';
-import {hasValidLocalCache} from '../store/movies.store';
 import {Alert} from 'react-native';
 import NetInfo from '@react-native-community/netinfo';
 
 type CheckBeforeDownloadProps = {
   onCancel: () => void;
-  onConfirm: (hasCache: boolean) => void;
+  onConfirm: () => void;
 }
 
 const CheckBeforeDownload = (props: CheckBeforeDownloadProps) => {
-  const check = async () => {
-    const hasCache = await hasValidLocalCache();
-    if (hasCache) {
-      props.onConfirm(true);
-    } else {
-      const netInfo = await NetInfo.fetch();
-      if (netInfo.details?.isConnectionExpensive) {
-        // warn the user if they're about to download ~2.5MB on data
-        Alert.alert(
-          'Download 3 MB on data?',
-          'It may take a few minutes on a slow connection.',
-          [
-            {text: 'Cancel', onPress: props.onCancel, style: 'cancel'},
-            {text: 'OK', onPress: () => props.onConfirm(false), style: 'default'}
-          ]
-        )
-      } else props.onConfirm(false);
-    }
-  };
-  check();
+  NetInfo.fetch().then(netInfo => {
+    if (netInfo.details?.isConnectionExpensive) {
+      // warn the user if they're about to download ~2.5MB on data
+      Alert.alert(
+        'Download 3 MB on data?',
+        'It may take a few minutes on a slow connection.',
+        [
+          {text: 'Cancel', onPress: props.onCancel, style: 'cancel'},
+          {text: 'OK', onPress: () => props.onConfirm(), style: 'default'}
+        ]
+      )
+    } else props.onConfirm();
+  });
   return null;
 };
 
