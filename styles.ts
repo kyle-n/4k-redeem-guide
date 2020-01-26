a405f25740c26ba38299a82e70fbd4d26834177c
kyle-n <kylebnazario@gmail.com>
2020-01-26 00:56:06 -0700

    Default light/dark styles based off Apple color UI guide

--- styles.ts
+++ styles.ts
@@ -1,3 +1,5 @@
 export const baseFontSize = 14;
-export const baseBackgroundColor = '#171717';
-export const baseColor = 'white';
+export const lightBackgroundColor = '#171717';
+export const darkBackgroundColor = 'rgb(44,44,46)'
+export const lightColor = 'black';
+export const darkColor = 'rgb(229,229,234)';
