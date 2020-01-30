149548948080bb7131c37da6defa46a5675e9353
kyle-n <kylebnazario@gmail.com>
2020-01-29 18:36:54 -0700

    Shared layouts

--- styles.ts
+++ styles.ts
@@ -1,3 +1,6 @@
+import {DynamicStyleSheet, DynamicValue} from 'react-native-dark-mode';
+import {StyleSheet} from 'react-native';
+
 export const baseFontSize = 14;
 export const lightBackgroundColor = '#171717';
 export const darkBackgroundColor: string = 'rgb(44,44,46)'
@@ -7,3 +10,13 @@ export const lightGray = 'rgb(142,142,147)';
 export const darkerLightGray = 'rgb(174,174,178)'
 export const darkLightGray = 'rgb(99,99,102)'
 export const contentGray = 'rgb(72,72,74)'
+
+export const sharedDynamicStyleSheet = new DynamicStyleSheet({
+  dynamicColor: {
+    backgroundColor: new DynamicValue(lightBackgroundColor, darkBackgroundColor),
+    color: new DynamicValue(lightColor, darkColor),
+  },
+  squareEntity: {
+    borderRadius: baseFontSize / 2
+  }
+});
