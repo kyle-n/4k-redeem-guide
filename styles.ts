9459eb73e97180596cb5632cb12b67b4b6cfa89f
kyle-n <kylebnazario@gmail.com>
2020-01-29 19:05:54 -0700

    Changes background color

--- styles.ts
+++ styles.ts
@@ -2,8 +2,8 @@ import {DynamicStyleSheet, DynamicValue} from 'react-native-dark-mode';
 import {StyleSheet} from 'react-native';
 
 export const baseFontSize = 14;
-export const lightBackgroundColor = '#171717';
-export const darkBackgroundColor: string = 'rgb(44,44,46)'
+export const lightBackgroundColor = 'rgb(242,242,247)';
+export const darkBackgroundColor = 'rgb(44,44,46)'
 export const lightColor = 'black';
 export const darkColor: string = 'rgb(229,229,234)';
 export const lightGray = 'rgb(142,142,147)';
