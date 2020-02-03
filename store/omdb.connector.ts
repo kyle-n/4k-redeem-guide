a064aedcac603ebee5da24418744a6c8cab8c768
kyle-n <kylebnazario@gmail.com>
2020-02-02 22:50:40 -0700

    Movie API connector

---
+++ store/omdb.connector.ts
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+interface SearchResponse {
+  page: number;
+  total_results: number;
+  total_pages: number;
+  results: [{
+    popularity: number;
+    vote_count: number;
+    video: boolean;
+    poster_path: string;
+    id: number;
+    adult: boolean;
+    backdrop_path: string;
+    original_language: string;
+    original_title: string;
+    genre_ids: number[];
+    title: string;
+    vote_average: number;
+    overview: string;
+    release_date: string;
+  }]
+}
+
+const apiKey = 'd3298570110878db366366b8e1f2f947';
+const baseSearchUrl = 'https://api.themoviedb.org/3/search/movie?';
+const baseImageUrl = 'https://image.tmdb.org/t/p/original';
+
+export const getMovieImage = async (title: string, year?: number): Promise<string | null> => {
+  const params = [
+    `api_key=${apiKey}`,
+    `t=${title}`,
+  ];
+  if (year) {
+    params.push(`y=${year}`);
+  }
+
+  const paramString = params.join('&');
+  const reqUrl = baseSearchUrl + paramString;
+  const resp: SearchResponse = (await axios.get(reqUrl)).data;
+  if (resp.total_results > 0) {
+    return baseImageUrl + resp.results[0].backdrop_path;
+  } else return null;
+};
