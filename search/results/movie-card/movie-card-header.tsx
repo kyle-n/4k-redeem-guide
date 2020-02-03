0ff49a6b27dfcb8a8393368346224bf2a6324e54
kyle-n <kylebnazario@gmail.com>
2020-02-02 23:44:02 -0700

    Splits out stylesheet and moves movie title to bottom of image

--- search/results/movie-card/movie-card-header.tsx
+++ search/results/movie-card/movie-card-header.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import {Body, CardItem, Left, Right, Text, Thumbnail} from 'native-base';
+import {Body, CardItem, Left, Right, Text} from 'native-base';
 import {Movie} from '../../../models';
 import {useDynamicStyleSheet} from 'react-native-dark-mode';
 import {baseFontSize, sharedDynamicStyleSheet} from '../../../styles';
 import {DropdownIcon} from '../../../shared-components';
-import {ImageBackground} from 'react-native';
+import {ImageBackground, StyleSheet} from 'react-native';
+
+const movieCardHeaderStyles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    width: '100%',
+    height: baseFontSize * 12
+  }
+});
 
 type MovieCardHeaderProps = {
   movie: Movie;
@@ -16,10 +26,9 @@ const MovieCardHeader = (props: MovieCardHeaderProps) => {
   const sharedStyles = useDynamicStyleSheet(sharedDynamicStyleSheet);
   return (
     <ImageBackground source={{uri: props.backgroundImgUrl}}
-                     style={{width: '100%', height: baseFontSize * 12}}>
+                     style={movieCardHeaderStyles.container}>
       <CardItem header style={[sharedStyles.squareEntity, {backgroundColor: 'rgba(0,0,0,0)'}]}>
         <Left>
-          <Thumbnail source={{uri: props.movie.imageUrl}} />
           <Body>
             <Text style={[sharedStyles.dynamicColor, {fontSize: baseFontSize * 1.5}]}>
               {props.movie.title}
