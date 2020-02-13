e69d24773fb5584aa77a25ab0cfd2a421ac1daae
kyle-n <kylebnazario@gmail.com>
2020-02-13 14:41:41 -0700

    Basic phone layout for external links

--- redeem-links/redeem-links.page.tsx
+++ redeem-links/redeem-links.page.tsx
@@ -1,14 +1,47 @@
 import React from 'react';
-import {Text, View} from 'native-base';
+import {Button, Icon, Text, View} from 'native-base';
+import {StyleSheet} from 'react-native';
+import {InfoLink} from '../search/results/movie-card/movie-card-text-info';
+import {baseFontSize} from '../styles';
 
-type RedeemLinksProps = {
+type RedeemLinksProps = {};
 
-};
+const redeemPageStyles = StyleSheet.create({
+  container: {
+    alignSelf: 'stretch',
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start'
+  },
+  buttonBox: {
+    margin: baseFontSize
+  }
+});
+
+const links: Array<{title: string, href: string}> = [
+  {title: 'Movies Anywhere', href: ''},
+  {title: 'iTunes', href: ''},
+  {title: 'Vudu', href: ''},
+  {title: 'FandangoNOW', href: ''},
+  {title: 'Amazon Video', href: ''},
+  {title: 'Microsoft', href: ''}
+];
 
 const RedeemLinks = (props: RedeemLinksProps) => {
   return (
-    <View>
-      <Text>Page</Text>
+    <View style={redeemPageStyles.container}>
+      {links.map(link => {
+        return (
+          <View key={link.title} style={redeemPageStyles.buttonBox}>
+            <Button
+                    info rounded iconLeft>
+              <Icon name="external-link" type="EvilIcons" />
+              <Text>{link.title}</Text>
+            </Button>
+          </View>
+        );
+      })}
     </View>
   )
 };
