87338debe471e5667bfa5ff0ddb7fe668db55634
kyle-n <kylebnazario@gmail.com>
2020-02-19 12:18:02 -0700

    Renames test files because I insisted on an app name that's not class-friendly

--- ios/4K4UTests/4K4UTests.m
+++ ios/4K4UTests/4K4UTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface 4K4UTests : XCTestCase
+@interface fourK4UTests : XCTestCase
 
 @end
 
-@implementation 4K4UTests
+@implementation fourK4UTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
