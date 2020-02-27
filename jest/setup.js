160fa2e4ac579d4005ee4720c98442af5c1e7558
kyle-n <kylebnazario@gmail.com>
2020-02-26 21:54:07 -0700

    Mocks safari IAB

--- jest/setup.js
+++ jest/setup.js
@@ -2,6 +2,7 @@ import {NativeModules} from 'react-native';
 import MockAsyncStorage from 'mock-async-storage';
 import RNCNetInfoMock from '@react-native-community/netinfo/jest/netinfo-mock.js';
 
+// mock dep for RN Dark Mode
 NativeModules.RNDarkMode = {
   initialMode: 'light',
   supportsDarkMode: true,
@@ -9,7 +10,16 @@ NativeModules.RNDarkMode = {
   removeListeners: jest.fn(),
 };
 
+// mock AsyncStorage
 const mockImpl = new MockAsyncStorage();
 jest.mock('@react-native-community/async-storage', () => mockImpl);
 
+// mock network info
 NativeModules.RNCNetInfo = RNCNetInfoMock;
+
+// mock safari IAB
+jest.mock('react-native-safari-view', () => {
+  return {
+    show: () => {}
+  }
+});
