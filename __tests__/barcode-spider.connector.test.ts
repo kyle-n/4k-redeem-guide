e2d407340ad13a0cc6ecd48e432dc2493d7c484f
kyle-n <kylebnazario@gmail.com>
2020-03-01 22:35:05 -0700

    Barcode spider API tests

--- __tests__/barcode-spider.connector.test.ts
+++ __tests__/barcode-spider.connector.test.ts
@@ -1,4 +1,5 @@
-import {transformToRegularTitle} from '../barcode-lookup/barcode-spider.connector';
+import {getMovieTitleFromBarcode, transformToRegularTitle} from '../barcode-lookup/barcode-spider.connector';
+import axios from 'axios';
 
 describe('barcode spider connector', () => {
 
@@ -10,6 +11,36 @@ describe('barcode spider connector', () => {
   it('strips brackets from titles', () => {
     const mockTitle = 'test [123] [4K]';
     expect(transformToRegularTitle(mockTitle)).toEqual('test');
+  });
+
+  it('returns null for no title in response', async () => {
+    const realFn = axios.get;
+    axios.get = jest.fn(() => {
+      return new Promise(resolve => {
+        resolve({data: {item_attributes: null}} as any);
+      });
+    });
+
+    const title = await getMovieTitleFromBarcode('123');
+
+    expect(title).toBeNull();
+
+    axios.get = realFn;
+  });
+
+  it('returns the title from the barcode API', async () => {
+    const realFn = axios.get;
+    axios.get = jest.fn(() => {
+      return new Promise(resolve => {
+        resolve({data: {item_attributes: {title: '123'}}} as any);
+      });
+    });
+
+    const title = await getMovieTitleFromBarcode('123');
+
+    expect(title).toBe('123');
+
+    axios.get = realFn;
   });
 
 });
