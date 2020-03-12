d732dbfe0725b9bc29cb0c51f602b17d1724d918
kyle-n <kylebnazario@gmail.com>
2020-03-11 22:12:18 -0600

    Layout for settings and support options

--- settings/settings.page.tsx
+++ settings/settings.page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Body, Container, Content, Left, ListItem, Right, Switch, Text, View} from 'native-base';
+import {Body, Container, Content, Icon, Left, ListItem, Right, Separator, Switch, Text, View} from 'native-base';
 import {DownloadIcon} from '../shared-components';
 
 type SettingsPageProps = {};
@@ -8,9 +8,13 @@ const SettingsPage = (props: SettingsPageProps) => {
   return (
     <Container>
       <Content>
+        <Separator bordered>
+          <Text>Downloads</Text>
+        </Separator>
+
         <ListItem icon>
           <Left>
-            <DownloadIcon />
+            <Icon name="radio-tower" type="Octicons" />
           </Left>
           <Body>
             <Text>Auto-download movies on data</Text>
@@ -19,6 +23,41 @@ const SettingsPage = (props: SettingsPageProps) => {
             <Switch value={false} />
           </Right>
         </ListItem>
+
+        <ListItem icon button>
+          <Left>
+            <Icon name="autorenew" type="MaterialIcons" />
+          </Left>
+          <Body>
+            <Text>Download movies now</Text>
+          </Body>
+          <Right>
+            <DownloadIcon />
+          </Right>
+        </ListItem>
+
+        <Separator bordered>
+          <Text>Support me</Text>
+        </Separator>
+
+        <ListItem button>
+          <Body>
+            <Text>$5 - A cup of coffee</Text>
+          </Body>
+        </ListItem>
+
+        <ListItem button>
+          <Body>
+            <Text>$10 - A really nice gift, thank you!</Text>
+          </Body>
+        </ListItem>
+        
+        <ListItem>
+          <Body>
+            <Text>$20 - My new favorite person</Text>
+          </Body>
+        </ListItem>
+
       </Content>
     </Container>
   );
