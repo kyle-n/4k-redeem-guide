1e1a39b14c088f4f55efd782852bf68b59af8d84
kyle-n <kylebnazario@gmail.com>
2020-03-11 22:15:41 -0600

    Splits optgroups into sub components

--- settings/settings.page.tsx
+++ settings/settings.page.tsx
@@ -8,58 +8,72 @@ const SettingsPage = (props: SettingsPageProps) => {
   return (
     <Container>
       <Content>
-        <Separator bordered>
-          <Text>Downloads</Text>
-        </Separator>
-
-        <ListItem icon>
-          <Left>
-            <Icon name="radio-tower" type="Octicons" />
-          </Left>
-          <Body>
-            <Text>Auto-download movies on data</Text>
-          </Body>
-          <Right>
-            <Switch value={false} />
-          </Right>
-        </ListItem>
-
-        <ListItem icon button>
-          <Left>
-            <Icon name="autorenew" type="MaterialIcons" />
-          </Left>
-          <Body>
-            <Text>Download movies now</Text>
-          </Body>
-          <Right>
-            <DownloadIcon />
-          </Right>
-        </ListItem>
-
-        <Separator bordered>
-          <Text>Support me</Text>
-        </Separator>
-
-        <ListItem button>
-          <Body>
-            <Text>$5 - A cup of coffee</Text>
-          </Body>
-        </ListItem>
-
-        <ListItem button>
-          <Body>
-            <Text>$10 - A really nice gift, thank you!</Text>
-          </Body>
-        </ListItem>
-        
-        <ListItem>
-          <Body>
-            <Text>$20 - My new favorite person</Text>
-          </Body>
-        </ListItem>
-
+        <DownloadsSettings />
+        <SupportOptions />
       </Content>
     </Container>
+  );
+};
+
+const DownloadsSettings = () => {
+  return (
+    <View>
+      <Separator bordered>
+        <Text>Downloads</Text>
+      </Separator>
+
+      <ListItem icon>
+        <Left>
+          <Icon name="radio-tower" type="Octicons" />
+        </Left>
+        <Body>
+          <Text>Auto-download movies on data</Text>
+        </Body>
+        <Right>
+          <Switch value={false} />
+        </Right>
+      </ListItem>
+
+      <ListItem icon button>
+        <Left>
+          <Icon name="autorenew" type="MaterialIcons" />
+        </Left>
+        <Body>
+          <Text>Download movies now</Text>
+        </Body>
+        <Right>
+          <DownloadIcon />
+        </Right>
+      </ListItem>
+    </View>
+  );
+};
+
+const SupportOptions = () => {
+  return (
+    <View>
+      <Separator bordered>
+        <Text>Support me</Text>
+      </Separator>
+
+      <ListItem button>
+        <Body>
+          <Text>$5 - A cup of coffee</Text>
+        </Body>
+      </ListItem>
+
+      <ListItem button>
+        <Body>
+          <Text>$10 - A really nice gift, thank you!</Text>
+        </Body>
+      </ListItem>
+
+      <ListItem>
+        <Body>
+          <Text>$20 - My new favorite person</Text>
+        </Body>
+      </ListItem>
+    </View>
   );
 };
 
