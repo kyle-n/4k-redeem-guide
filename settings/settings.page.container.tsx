830e53ec93e3d6e64fc26b012d14e96cb0dfae1a
kyle-n <kylebnazario@gmail.com>
2020-03-21 11:18:11 -0600

    Loads products on settings page

--- settings/settings.page.container.tsx
+++ settings/settings.page.container.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import SettingsPage from './settings.page';
+import * as RNIap from 'react-native-iap';
+import getSkus from './iap/init-iaps';
+import {Product} from 'react-native-iap';
 
 type SettingsPageContainerProps = {};
 
 const SettingsPageContainer = (props: SettingsPageContainerProps) => {
+  const [products, setProducts] = useState([] as Product[]);
+  RNIap.getProducts(getSkus()).then((loadedProducts: Product[]) => setProducts(loadedProducts));
   return (
     <SettingsPage />
   );
