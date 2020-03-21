fe4597b19f692060fb3a13645547e35c9e621cf0
kyle-n <kylebnazario@gmail.com>
2020-03-21 11:11:38 -0600

    Inits iap skus

---
+++ settings/iap/init-iaps.ts
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Platform} from 'react-native';
+
+const itemSkus = Platform.select({
+  ios: [
+    'supportDevFor5',
+    'supportDevFor10',
+    'supportDevFor20',
+  ], android: [
+    'support_dev_for_5',
+    'support_dev_for_10',
+    'support_dev_for_20'
+  ]
+});
+
+const getSkus = (): string[] => itemSkus as string[];
