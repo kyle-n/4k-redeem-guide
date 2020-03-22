8faf215035142bf2ba6545ca294b4afc52f2cd7d
kyle-n <kylebnazario@gmail.com>
2020-03-22 10:05:45 -0600

    Comments out test purchase ids

--- settings/iap/init-iaps.tsx
+++ settings/iap/init-iaps.tsx
@@ -16,22 +16,22 @@ const mapDispatchToProps = {
 export const purchases = [
   {
     ios: 'supportDevFor5',
-    // android: 'support_dev_for_5',
-    android: 'android.test.purchased',
+    android: 'support_dev_for_5',
+    // android: 'android.test.purchased',
     storeKey: 'five',
     userFacingText: '$5 - A cup of coffee'
   },
   {
     ios: 'supportDevFor10',
-    // android: 'support_dev_for_10',
-    android: 'android.test.canceled',
+    android: 'support_dev_for_10',
+    // android: 'android.test.canceled',
     storeKey: 'ten',
     userFacingText: '$10 - A really nice gift, thank you!'
   },
   {
     ios: 'supportDevFor20',
-    // android: 'support_dev_for_20',
-    android: 'android.test.item_unavailable',
+    android: 'support_dev_for_20',
+    // android: 'android.test.item_unavailable',
     storeKey: 'twenty',
     userFacingText: '$20 - A true patron of movies and indie app development'
   }
